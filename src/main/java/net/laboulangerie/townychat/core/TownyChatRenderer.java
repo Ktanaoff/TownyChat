f904f164ffd3bafef5dd28404acab0fb9ba56e0d
Valdrecus <JPRODRIGUEZ.SALVADOR@GMAIL.COM>
2025-06-24 23:35:41 -0500

    Update TownyChatRenderer.java

--- src/main/java/net/laboulangerie/townychat/core/TownyChatRenderer.java
+++ src/main/java/net/laboulangerie/townychat/core/TownyChatRenderer.java
@@ -1,10 +1,6 @@
 package net.laboulangerie.townychat.core;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -19,31 +15,21 @@ import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.laboulangerie.townychat.TownyChat;
 import net.laboulangerie.townychat.player.ChatPlayer;
 import net.laboulangerie.townychat.player.ChatPlayerManager;
 
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Resident;
+import com.palmergames.bukkit.towny.object.Town;
+
 public class TownyChatRenderer implements ChatRenderer.ViewerUnaware {
-    private ChatPlayerManager chatPlayerManager;
-    private ComponentRenderer componentRenderer;
-    private Map<String, TagResolver> resolverMap = Map.of(
-            "townychat.format.color", StandardTags.color(),
-            "townychat.format.bold",
-            TagResolver.resolver(Set.of("bold", "b"), (argumentQueue, context) -> Tag.styling(TextDecoration.BOLD)),
-            "townychat.format.underline",
-            TagResolver.resolver(Set.of("underline", "u"),
-                    (argumentQueue, context) -> Tag.styling(TextDecoration.UNDERLINED)),
-            "townychat.format.italic",
-            TagResolver.resolver(Set.of("italic", "i", "em"),
-                    (argumentQueue, context) -> Tag.styling(TextDecoration.ITALIC)),
-            "townychat.format.strikethrough",
-            TagResolver.resolver(Set.of("strikethrough", "st"),
-                    (argumentQueue, context) -> Tag.styling(TextDecoration.STRIKETHROUGH)),
-            "townychat.format.obfuscated",
-            TagResolver.resolver(Set.of("obfuscated", "obf"),
-                    (argumentQueue, context) -> Tag.styling(TextDecoration.OBFUSCATED)),
-            "townychat.format.rainbow", StandardTags.rainbow());
+
+    private final ChatPlayerManager chatPlayerManager;
+    private final ComponentRenderer componentRenderer;
 
     public TownyChatRenderer() {
         this.chatPlayerManager = TownyChat.PLUGIN.getChatPlayerManager();
@@ -51,44 +37,86 @@ public class TownyChatRenderer implements ChatRenderer.ViewerUnaware {
     }
 
     private Component buildMessage(@NotNull Player source, Component message, String channelFormat) {
-        String plainText = PlainTextComponentSerializer.plainText().serialize(message);
-        censorString(plainText);
+        String raw = PlainTextComponentSerializer.plainText().serialize(message);
+        Component legacyParsed = LegacyComponentSerializer.legacyAmpersand().deserialize(raw);
 
-        List<TagResolver> resolvers = new ArrayList<>();
-        resolverMap.forEach((perm, resolver) -> {
-            if (source.hasPermission(perm)) {
-                resolvers.add(resolver);
-            }
-        });
+        String cleanRaw = censorString(PlainTextComponentSerializer.plainText().serialize(legacyParsed));
 
+        TagResolver.Builder tagBuilder = TagResolver.builder();
         if (source.hasPermission("townychat.format.all")) {
-            message = MiniMessage.miniMessage().deserialize(plainText);
+            tagBuilder.resolver(StandardTags.defaults());
         } else {
-            message = MiniMessage.builder()
-                    .tags(TagResolver.builder().resolvers(resolvers).build())
-                    .build()
-                    .deserialize(plainText);
+            if (source.hasPermission("townychat.format.colors"))
+                tagBuilder.resolver(StandardTags.color());
+            if (source.hasPermission("townychat.format.decorations"))
+                tagBuilder.resolver(StandardTags.decorations());
+            if (source.hasPermission("townychat.format.misc")) {
+                tagBuilder.resolver(StandardTags.clickEvent());
+                tagBuilder.resolver(StandardTags.hoverEvent());
+                tagBuilder.resolver(StandardTags.insertion());
+                tagBuilder.resolver(StandardTags.keybind());
+                tagBuilder.resolver(StandardTags.translatable());
+                tagBuilder.resolver(StandardTags.rainbow());
+                tagBuilder.resolver(StandardTags.gradient());
+                tagBuilder.resolver(StandardTags.transition());
+                tagBuilder.resolver(StandardTags.font());
+                tagBuilder.resolver(StandardTags.newline());
+            }
+        }
+        Component parsedMessage = MiniMessage.builder()
+                .tags(tagBuilder.build())
+                .build()
+                .deserialize(cleanRaw);
+
+        tagBuilder.resolver(Placeholder.component("message", parsedMessage));
+        tagBuilder.resolver(Placeholder.component("username", source.name()));
+
+        String townFormat = TownyChat.PLUGIN.getConfig().getString("papi_chat_formatting.town", "");
+        String nationFormat = TownyChat.PLUGIN.getConfig().getString("papi_chat_formatting.nation", "");
+        String bothFormat = TownyChat.PLUGIN.getConfig().getString("papi_chat_formatting.both", "");
+
+        Component formattedTown = Component.empty();
+        Component formattedNation = Component.empty();
+        Component formattedBoth = Component.empty();
+
+        Resident resident = TownyAPI.getInstance().getResident(source);
+        if (resident != null && resident.hasTown()) {
+            Town town = resident.getTownOrNull();
+            if (town != null) {
+                formattedTown = MiniMessage.miniMessage().deserialize(townFormat.replace("%s", town.getName()));
+
+                if (town.hasNation()) {
+                    Nation nation = town.getNationOrNull();
+                    if (nation != null) {
+                        formattedNation = MiniMessage.miniMessage().deserialize(nationFormat.replace("%s", nation.getName()));
+                        formattedBoth = MiniMessage.miniMessage().deserialize(
+                                bothFormat.replace("%t", town.getName()).replace("%n", nation.getName()));
+                    } else {
+                        formattedBoth = formattedTown;
+                    }
+                } else {
+                    formattedBoth = formattedTown;
+                }
+            }
         }
 
-        resolvers.add(Placeholder.component("message", message));
-        resolvers.add(Placeholder.component("username", source.name()));
+        tagBuilder.resolver(Placeholder.component("town", formattedTown));
+        tagBuilder.resolver(Placeholder.component("nation", formattedNation));
+        tagBuilder.resolver(Placeholder.component("nation_town", formattedBoth));
 
-        return componentRenderer.parse(source, channelFormat, TagResolver.resolver(resolvers));
+        return componentRenderer.parse(source, channelFormat, tagBuilder.build());
     }
 
     @Override
-    public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName,
-            @NotNull Component message) {
+    public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName, @NotNull Component message) {
         ChatPlayer chatPlayer = chatPlayerManager.getChatPlayer(source);
         String channelFormat = chatPlayer.getCurrentChannel().getFormat();
-
         return buildMessage(source, message, channelFormat);
     }
 
     public @NotNull Component spyRender(@NotNull Player source, @NotNull Component message) {
         ChatPlayer chatPlayer = chatPlayerManager.getChatPlayer(source);
         String channelFormat = chatPlayer.getCurrentChannel().getSpyFormat();
-
         return buildMessage(source, message, channelFormat);
     }
 
@@ -97,11 +125,9 @@ public class TownyChatRenderer implements ChatRenderer.ViewerUnaware {
         String[] censorChars = { "#", "@", "!", "*" };
 
         for (String word : words) {
-            // Not readable to say the least but i like it
-            // It generates a random string e.g. insult -> !#@!*#
-            // Yes it's overcomplicated but it looks cool :)
             string = string.replaceAll("(?i)" + Pattern.quote(word),
-                    new Random().ints(word.length(), 0, censorChars.length).mapToObj(i -> censorChars[i])
+                    new Random().ints(word.length(), 0, censorChars.length)
+                            .mapToObj(i -> censorChars[i])
                             .collect(Collectors.joining()));
         }
 
